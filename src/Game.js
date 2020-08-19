8df958485c078daf8a6cbfddd4fae86425a33146
devkaush <devkaush123@gmail.com>
2020-08-19 16:59:23 +0530

    draw game

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Board from './Board'
 
+const { calculateWinner, checkDraw } = require('./gameStatus')
+
 export default function Game() {
     const [ticTac, setTicTac] = useState({
         history: [ { squares: Array(9).fill(null) } ], 
@@ -20,6 +22,7 @@ export default function Game() {
     const history = ticTac.history
     const current = history[ticTac.stepNumber]
     const winner = calculateWinner(current.squares)
+    const draw = checkDraw(current.squares)
 
     function jumpTo(step){
         setTicTac({history: history, stepNumber: step, xIsNext: (step%2) === 0})
@@ -28,6 +31,8 @@ export default function Game() {
     let status
     if(winner){
         status = "Winner is: " + winner
+    }else if(draw){
+        status = "Game is a draw"
     }else{
         status = "Next Player is: " + (ticTac.xIsNext ? 'X': 'O')
     }
@@ -66,23 +71,4 @@ export default function Game() {
         </div>
     )
 
-    function calculateWinner(squares) {
-        const lines = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6]
-        ];
-        for (let i = 0; i < lines.length; i++) {
-          const [a, b, c] = lines[i];
-          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-          }
-        }
-        return null;
-      }
 }
